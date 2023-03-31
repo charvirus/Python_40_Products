fb8e5f8fa28fba0511ca3b021ec547d73aefd94f
charvirus <jbrandon413@naver.com>
2023-03-31 17:58:33 +0900

    #5 프로젝트 3 끝

---
+++ 3. 텍스트를 음성으로 변환/main3-3.py
@@ -0,0 +1,20 @@
+from gtts import gTTS
+from playsound import playsound
+import os
+
+# 경로를 .py파일의 실행경로로 이동, 현재 경로로 이동
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+print(os.chdir(os.path.dirname(os.path.abspath(__file__))))
+print(os.path.dirname(os.path.abspath(__file__)))
+print(os.path.abspath(__file__))
+
+file_path = '나의 텍스트.txt'
+
+with open(file_path, 'rt', encoding='UTF8') as f:
+    read_file = f.read()
+
+tts = gTTS(text=read_file, lang='ko')
+
+tts.save("Hi.mp3")
+
+playsound("Hi.mp3")
