7e0baa683de537be88934f0cdea6908eeffd06ce
charvirus <jbrandon413@naver.com>
2024-01-11 16:37:42 +0900

    #14 다음주 전까지 GUI 만들자

---
+++ 12. 엑셀의 정보를 불러와 수료증 자동생성/main12-1.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import os
+df = pd.DataFrame(
+    [["홍길동", "1990.01.02", "2021-0001"], ["김민준", "1990.05.06", "2021-0002"], ["김철수", "2000.08.08", "2021-0003"],
+     ["김영희", "2000.09.09", "2021-0004"], ["이서준", "2010.10.10", "2021-0005"], ["장다인", "2017.12.12", "2021-0006"]])
+
+print(df)
+print(os.path.dirname(os.path.abspath(__file__)))
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+df.to_excel(r'수료증명단.xlsx',index=False,header=False)
